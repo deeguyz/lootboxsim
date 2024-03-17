d9f439094b756bf43209d23b9b018590e08ae011
deeguyz <alchemist4lyfe@gmail.com>
2024-03-16 18:46:14 -0700

    Updated docker compose to create test table with values

---
+++ database/dataset/query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE test(
+    id int PRIMARY KEY,
+    username TEXT
+);
+
+INSERT INTO test(id, username) VALUES
+(56, 'testuser1'),
+(613, 'testuser2'),
+(273, 'testuser3'),
+(534, 'testuser4'),
+(72, 'testuser5');
