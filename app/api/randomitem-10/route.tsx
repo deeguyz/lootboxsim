7206932f46f4caf8fe95a77c0fd4b9a0d83bcd28
deeguyz <alchemist4lyfe@gmail.com>
2024-03-21 15:31:11 -0700

    Added insert to inventory for pull 10 items

--- app/api/randomitem-10/route.tsx
+++ app/api/randomitem-10/route.tsx
@@ -1,8 +1,11 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/route';
 
 import prisma from '../../../lib/prisma';
 
 export async function GET(req: NextRequest) {
+  const data = await getServerSession(authOptions);
   try {
     // Fetch all items from the database (cache this later on but for now just get it working)
     const allItems = await prisma.items.findMany();
@@ -11,6 +14,12 @@ export async function GET(req: NextRequest) {
 
     // Get total weight of all items
     const totalWeight = allItems.reduce((acc: number, item: any) => acc + item.item_weight, 0);
+
+    const inventoryIdResult: string[] = await prisma.$queryRaw`
+    SELECT inventory.id 
+    FROM inventory 
+    INNER JOIN users ON users.id = inventory.user_id 
+    WHERE users.id = ${data.user.id}::uuid`;
 
     for (let i = 0; i < 10; i++) {
       const randomNumber = Math.random() * totalWeight;
@@ -29,6 +38,19 @@ export async function GET(req: NextRequest) {
 
       if (selectedItem) {
         selectedItems.push(selectedItem);
+      }
+
+      try {
+        const inventoryId = inventoryIdResult[0].id;
+
+        const updateInventory = await prisma.$queryRaw`
+          INSERT INTO inventory_items (inventory_id, item_id, quantity)
+          VALUES (${inventoryId}::uuid, ${selectedItems[i].id}::uuid, 1)
+          ON CONFLICT (inventory_id, item_id)
+          DO UPDATE SET quantity = inventory_items.quantity + 1;
+        `;
+      } catch (error) {
+        console.log('randomitem', error.message);
       }
     }
 
